7cab4ad79c28f88b3359f3b9fae0e1ef4fe3fc8d
William Kwao <williamkwao@gmail.com>
2019-01-11 00:52:20 -0500

    added header mobile style and toggle icon

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,32 +3,54 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 import logo from './yaplogo.png'
+import toggleIcon from './toggleIcon.png'
 
-const YapHeader = styled.div`
+const YapHeader = styled.nav`
   background: transparent;
   margin-bottom: 1.45rem;
   display: flex;
   justify-content: space-between;
+  padding: 26px 8px;
 
   h1 a {
     color: black;
     text-decoration: none;
   }
   #logo {
-    width: 198px;
+    width: 140px;
     margin-bottom: 0px;
   }
   li,
   ul {
-    display: inline-block;
+    display: none;
     vertical-align: middle;
   }
-  ul {
-    margin: auto;
-    margin-right: 10px;
+
+  #nav-toggle {
+    width: 32px;
+    margin: auto 10px;
   }
-  li {
-    padding: 21px;
+
+  /* Dextop Styles */
+  @media (min-width: 992px) {
+    #logo {
+      width: 198px;
+    }
+    #nav-toggle {
+      display: none;
+    }
+    li,
+    ul {
+      display: inline-block;
+      vertical-align: middle;
+    }
+    ul {
+      margin: auto;
+      margin-right: 10px;
+    }
+    li {
+      padding: 21px;
+    }
   }
 `
 const Header = ({ siteTitle }) => (
@@ -41,6 +63,7 @@ const Header = ({ siteTitle }) => (
       <li> SPEAKER'S CORNER</li>
       <li> UPCOMING EVENT</li>
     </ul>
+    <img id="nav-toggle" src={toggleIcon} alt="mobile menu toggle" />
   </YapHeader>
 )
 
