fc6a3d56190742709dbb853e096ace5c55c883a0
William Kwao <williamkwao@gmail.com>
2019-01-15 00:34:29 -0500

    Added slider component and started work on the landing section of front page

---
+++ src/components/slider/slider.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+import { default as SlickSlider } from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
+import slide1 from '../../images/yap-meeting1.png'
+import slide2 from '../../images/yap-meeting.jpg'
+
+const YapSlider = styled.div`
+  .slick-track {
+    .slide-div {
+      /* height: 400px !important; */
+
+      img {
+        width: 100%;
+        object-fit: cover;
+        height:400px;
+      }
+      }
+    }
+  }
+`
+const Slider = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 5000,
+  }
+  return (
+    <YapSlider>
+      <SlickSlider {...settings}>
+        <div className="slide-div">
+          <img src={slide1} alt="slide" />
+        </div>
+        <div className="slide-div" alt="slide">
+          <img src={slide2} />
+        </div>
+      </SlickSlider>
+    </YapSlider>
+  )
+}
+
+export default Slider
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,49 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import styled from 'styled-components'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Slider from '../components/slider/slider'
 
+const LandingSection = styled.section`
+  /* Dextop Styles */
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: 400px 700px;
+  }
+`
+const YapMission = styled.div`
+  h1 {
+    color: #fcbc19;
+  }
+
+  a {
+    color: #000;
+  }
+  a:hover {
+    color: #fcbc19;
+  }
+
+  @media (min-width: 992px) {
+    margin-right: 20px;
+  }
+`
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`YAP`, 'Young African Professionals']} />
+    <LandingSection>
+      <YapMission>
+        <h1>What We Do</h1>
+        <p>
+          We provide connections to invauable resources through our network,
+          facilitate professional development in workshops.
+        </p>
+        <a href="#">
+          <p>Learn More About YAP’s Offerings</p>
+        </a>
+      </YapMission>
+      <Slider />
+    </LandingSection>
   </Layout>
 )
 
