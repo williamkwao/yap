32d0552279f9929dd0adb3232c59a9cf90d3d38c
William Kwao <williamkwao@gmail.com>
2019-01-26 19:15:12 -0500

    added become a member section to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 import { TeamCardsLayout, Layout, Slider, SEO, Events } from '../components'
 import SVGIcon from '../components/SVGIcon'
+import yapAppImg from '../images/yapp-app.png'
 
 const LandingSection = styled.section`
   overflow-x: hidden;
@@ -70,6 +71,47 @@ const EventsSection = styled.section`
     }
   }
 `
+
+const MemberSection = styled.section`
+  padding: 15px;
+  display: grid;
+  font-family: georgia, serif;
+  :hover {
+    h2 {
+      color: #fcbc19;
+    }
+  }
+  h2 {
+    transition: 0.5s;
+  }
+  p {
+    padding-bottom: 20px;
+  }
+  button {
+    :focus {
+      border: none;
+    }
+    :hover {
+      background-color: #fcbc19;
+      box-shadow: -2px 4px 4px rgba(0, 0, 0, 0.25);
+    }
+    font-size: 18px;
+    background: #000;
+    color: #fff;
+    padding: 19px 52px;
+    transition: 0.5s;
+  }
+  .yap-app-screenshot {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    padding: 80px 35px;
+    grid-template-columns: 35% 65%;
+    .yap-app-screenshot {
+      display: block;
+    }
+  }
+`
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`YAP`, 'Young African Professionals']} />
@@ -118,6 +160,23 @@ const IndexPage = () => (
       <h1>Upcoming Events</h1>
       <Events />
     </EventsSection>
+    <MemberSection>
+      <div>
+        <h2>Become a Member of our YAP APP</h2>
+        <p>
+          We are proud to announce our NEW professional networking app for our
+          YAP DC Membership. With over 10,000 members to date, we are excited to
+          provide an innovative platform to continue to connect our community
+          for career development opportunities.
+        </p>
+        <a href="https://yapdc.mn.co">
+          <button>GET THE APP</button>
+        </a>
+      </div>
+      <div className="yap-app-screenshot">
+        <img src={yapAppImg} alt="Screen shot of mobile app" />
+      </div>
+    </MemberSection>
     <section>
       <TeamCardsLayout />
     </section>
