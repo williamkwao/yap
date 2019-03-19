f6ccf5fb1115538947d83227dffa8d1c2957a2d5
William Kwao <williamkwao@gmail.com>
2019-03-19 08:42:59 -0400

    Get about content from markdown conversion to html (#64)
    * configuration for leadership cms preview
    
    * Update Home Page “leadershp”
    
    * Leadership preview and additin of header to leadership data
    
    * fix lerdership for about page and add header to cms
    
    * added sponsors markdown file
    
    * CMS Schema config for sponsors
    
    * Upload “/static/img/wave.jpg”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/mansa.jpg”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/kpmg.png”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/jk-logo.jpg”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/io-spaces.png”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/harvest-logo.png”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/dd.png”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/axn.png”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/afrinection-logo.png”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/yep-logo.jpg”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/star-white-background-1-.jpg”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/newwave_foundation_primary_magenta-purple_gradient.png”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/nsl-logo.jpg”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/moaa-logo.jpg”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/georgetown_s_sfs_bluergb.jpg”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/gu_msob.vert_rev-with-bluergb-1-.jpg”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/fs-circumspecte-id-logo-color-2017-1.png”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/engine-blox_logo-blue.png”
    
    * Update Home Page “sponsors”
    
    * Upload “/static/img/atigs-2018-logo.png”
    
    * Update Home Page “sponsors”
    
    * Update Home Page “sponsors”
    
    * fetcing data from markdown
    
    * Update Home Page “sponsors”
    
    * added georgetown business school url
    
    * remove unused code
    
    * added nheader to sponsor component
    
    * sponsorship cms preview
    
    * sponsorship cms preview
    
    * about page markdown file
    
    * Upload “/static/img/diaspora1.jpg”
    
    * Update About Page “about”
    
    * Update About Page “about”
    
    * Update About Page “about”
    
    * cms schema for about page
    
    * Update About Page “about”
    
    * Update About Page “about”
    
    * Update About Page “about”
    
    * new md file
    
    * Update About Page “about”
    
    * commit get about content from markdown conversion to html

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import remark from 'remark'
+import recommended from 'remark-preset-lint-recommended'
+import remarkHtml from 'remark-html'
 import { Layout, TeamCardsLayout } from '../components'
 import styled from 'styled-components'
-import diasporaImg from '../images/diaspora1.jpg'
 
 const AboutStyle = styled.section`
   line-height: 1.5;
@@ -36,8 +38,10 @@ const AboutStyle = styled.section`
     padding-left: 15px;
     padding-right: 15px;
 
-    h3 {
+    h3,
+    h2 {
       margin-bottom: 10px;
+      font-size: 1.38316rem;
     }
     ul {
       margin-left: 0px;
@@ -56,6 +60,11 @@ const AboutStyle = styled.section`
     }
 
     .team {
+    }
+  }
+  .content2 {
+    li {
+      padding-bottom: 3px !important;
     }
   }
 
@@ -85,113 +94,40 @@ const AboutStyle = styled.section`
 const About = props => {
   const data = props.data
   const leadershipData = data.leadership.childMarkdownRemark.frontmatter
+  const content = data.content.childMarkdownRemark.frontmatter
+  const html1 = remark()
+    .use(recommended)
+    .use(remarkHtml)
+    .processSync(content.content1)
+    .toString()
+  const html2 = remark()
+    .use(recommended)
+    .use(remarkHtml)
+    .processSync(content.content2)
+    .toString()
+
   return (
     <Layout>
       <AboutStyle>
         <section className="banner-section">
-          <img className="banner-image" src={diasporaImg} alt="Yap Banner" />
+          <img
+            className="banner-image"
+            src={content.coverImage}
+            alt="Yap Banner"
+          />
         </section>
-        <section className="text-section">
-          <h1>WHAT WE DO</h1>
-          <p>
-            As the premier conveners of events and opportunities for young
-            African professionals in the diaspora, our mission is to connect
-            young African diaspora professionals to career development and
-            opportunity creation. We fulfill this mission providing timely and
-            relevant resources, facilitating innovative professional development
-            experiences, fostering inspirational networking, and encouraging
-            impactful community building. These goals advance our vision to
-            address the professional needs of young Africans in the diaspora;
-            support more Africans in positions of power and influence; and
-            positively influence narratives about the continent.
-          </p>
-          <ul>
-            <li>
-              <h3>Provide Resources</h3>
-              <p>
-                We are excited about the growth of our network over the years.
-                Whether you are a budding entrepreneur, a recent graduate, or a
-                seasoned professional, you can enjoy preferred access to
-                tailored professional development resources and career-building
-                opportunities available on our various online platforms.
-              </p>
-            </li>
-            <li>
-              <h3>Facilitate Professional Development</h3>
-              <p>
-                Whether you are considering an advanced degree or looking to
-                advance in the workplace, our curated workshops and networking
-                events provide special access to unique professional development
-                opportunities, help you launch entrepreneurial aspirations and
-                advance in your career.
-              </p>
-            </li>
-            <li>
-              <h3>Foster Networking and Encourage Community Building</h3>
-              <p>
-                Our online networking platform can help you elevate your
-                networking by connecting you to other ambitious, driven, young
-                diaspora professionals like you! This space is for you to
-                continue conversations after an event, meet people with similar
-                interests, and build communities of support and
-                collaboration.&nbsp;
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://yapdc.mn.co"
-                >
-                  Become a member today!
-                </a>
-              </p>
-            </li>
-          </ul>
-        </section>
-        <section className="text-section">
-          <h1>WHO WE ARE</h1>
-          <li>
-            <p>
-              YAP DC has always been operated by dedicated and highly skilled
-              volunteers. The public and our members are our clients, and we
-              treat them as such. In late 2018, we stood up a Board of Directors
-              and re-structured our Operations Team to maintain YAP DC’s
-              501(c)(3) status and manage operational transitions from year to
-              year. The Board is the highest governing body of the organization,
-              responsible for establishing the vision, mission and overall
-              direction of the organization. Let’s meet the team.
-            </p>
-          </li>
-        </section>
+        <section
+          className="text-section"
+          dangerouslySetInnerHTML={{ __html: html1 }}
+        />
+
         <section className="text-section team">
           <TeamCardsLayout data={leadershipData} />
         </section>
-        <section className="text-section">
-          <h1>OPERATIONS TEAM</h1>
-          <li>
-            <p>
-              The operations team consists of volunteers who serve as the core
-              leaders in envisioning and executing the day to day matters in
-              line with the strategic priorities of the organization. Working
-              with YAP DC is an invaluable and unique experience. If you are
-              interested in being a member of the team, you can view our vacant
-              positions &nbsp;
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.idealist.org/en/nonprofit/b55e85ab2beb4d85b447f4092f56e754-young-african-professionals-dc-yap-dc-washington"
-              >
-                here.
-              </a>
-            </p>
-            <h3>MEMBERS</h3>
-            <p> Rose Essiem </p>
-            <p> Rumbidzai Mufuka</p>
-            <p> Fatmatta Wurie </p>
-            <p> Sika Dunyoh </p>
-            <p> Chinwe Emeruem</p>
-            <p> Thelma Ekeocha </p>
-            <p> Cornelia Poku</p>
-          </li>
-        </section>
+        <section
+          className="text-section content2"
+          dangerouslySetInnerHTML={{ __html: html2 }}
+        />
       </AboutStyle>
     </Layout>
   )
@@ -212,6 +148,16 @@ export const query = graphql`
             image
             description
           }
+        }
+      }
+    }
+
+    content: file(name: { eq: "about-content" }) {
+      childMarkdownRemark {
+        frontmatter {
+          coverImage
+          content1
+          content2
         }
       }
     }
