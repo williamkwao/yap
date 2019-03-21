3ef8ad6b313540c1e5454dd08ca52b45a3d8bb11
William Kwao <williamkwao@gmail.com>
2019-03-20 22:25:57 -0400

    cleanup for review

--- src/cms/preview-templates/aboutPreview.js
+++ src/cms/preview-templates/aboutPreview.js
@@ -5,7 +5,6 @@ const AboutPreview = ({ entry }) => {
   const coverImage = entry.getIn(['data', 'coverImage'])
   const content1 = entry.getIn(['data', 'content1'])
   const content2 = entry.getIn(['data', 'content2'])
-  console.log('message', content1, content2)
   return (
     <AboutTemplate coverImage={coverImage} html1={content1} html2={content2} />
   )
